9b9bcaab7530d4272a40a0dc1eb21ce50ae30ccb
Jacob Yoo <bracket0723@gmail.com>
2022-04-04 22:52:31 +0900

    Initial commit

---
+++ app/src/main/java/kr/yooreka/automationsample1/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.yooreka.automationsample1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.automationsample1.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
